da24fd0f21be0393458cc2ad264f1774334c13aa
Yousef Abuaqel <146733467+usfaql@users.noreply.github.com>
2024-09-18 05:28:10 +0300

    Update server.js1

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const phoneRouter = require('./routes/Phones');
 
 
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['https://serverwabulk.onrender.com'],
   credentials: true
 }));
 
